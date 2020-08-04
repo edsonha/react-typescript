635b7bcf7727702144180867720df74f27535aff
edsonha <edsonha@gmail.com>
2020-08-04 23:14:21 +0800

    Make QuestionCard component a React.FC Functional Component that take in Props with specified types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => <div>Question Card</div>;
 
 export default QuestionCard;
